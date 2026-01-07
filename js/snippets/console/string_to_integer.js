9c5e9b4a7f4407d5c68ffdad6438e5fae16a1eb1
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-01-07 16:42:35 +0100

    start new project

---
+++ js/snippets/console/string_to_integer.js
@@ -0,0 +1,4 @@
+const letter1 = Number(prompt("Nenne deine erste Zahl:"));
+const letter2 = parseInt(prompt("Nenne deine zweite Zahl:"));
+
+console.log("Deine Zahlen addiert ergeben: " + letter1 + letter2);
--- main.js
+++
@@ -1,4 +0,0 @@
-const projectName = "LearnLoop Task";
-const projetVersion = "1.0.0";
-let currentStatus = "initial";
-let countTasks = 5;
