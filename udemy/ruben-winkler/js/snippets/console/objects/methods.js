54afa642d1c58d124070bb29a5de9554bbe4fc55
Sven <you@example.com>
2026-01-13 19:43:32 +0100

    learn return-Method

--- udemy/ruben-winkler/js/snippets/console/objects/methods.js
+++ udemy/ruben-winkler/js/snippets/console/objects/methods.js
@@ -15,33 +15,25 @@ const bankAccount = {
   depositMoney() {
     let depositService = Number(prompt("Wieviel Geld möchten Sie einzahlen?"));
     if (depositService >= 10000) {
-      console.log(
-        `Das tut mir Leid, diese Summe (${depositService}€) muss aus gesetzlichen Gründen (Verdacht auf Geldwäsche) von einem Mitarbeiter durchgeführt werden!`
-      );
+      return `Das tut mir Leid, diese Summe (${depositService}€) muss aus gesetzlichen Gründen (Verdacht auf Geldwäsche) von einem Mitarbeiter durchgeführt werden!`;
     } else {
       this.balance += depositService;
-      console.log(
-        `Vielen Dank, der Service wurde ausgeführt. Ihr neues Guthaben beträgt nun: ${this.balance}€`
-      );
+      return `Vielen Dank, der Service wurde ausgeführt. Ihr neues Guthaben beträgt nun: ${this.balance}€`;
     }
   },
 
   withdrawMoney() {
     let drawService = Number(prompt("Wieviel Geld möchten Sie abheben?"));
     if (drawService > this.balance) {
-      console.log(
-        `Es tut mir leid, aber Sie möchten mehr Geld abheben, als Sie besitzen. Sie können maximal ${this.balance}€ abheben.`
-      );
+      return `Es tut mir leid, aber Sie möchten mehr Geld abheben, als Sie besitzen. Sie können maximal ${this.balance}€ abheben.`;
     } else {
       this.balance -= drawService;
-      console.log(
-        `Vielen Dank - Das Geld wurde abgebucht und Ihr neuer Kontostand beträgt nun: ${this.balance}€.`
-      );
+      return `Vielen Dank - Das Geld wurde abgebucht und Ihr neuer Kontostand beträgt nun: ${this.balance}€.`;
     }
   },
 
   getBalance() {
-    console.log(`Ihr aktuelles Guthaben beträgt: ${this.balance}€`);
+    return `Ihr aktuelles Guthaben beträgt: ${this.balance}€`;
   },
 };
 
@@ -52,33 +44,25 @@ const savingsAccount = {
   depositMoney() {
     let depositService = Number(prompt("Wieviel Geld möchten Sie einzahlen?"));
     if (depositService >= 10000) {
-      console.log(
-        `Das tut mir Leid, diese Summe (${depositService}€) muss aus gesetzlichen Gründen (Verdacht auf Geldwäsche) von einem Mitarbeiter durchgeführt werden!`
-      );
+      return `Das tut mir Leid, diese Summe (${depositService}€) muss aus gesetzlichen Gründen (Verdacht auf Geldwäsche) von einem Mitarbeiter durchgeführt werden!`;
     } else {
       this.balance += depositService;
-      console.log(
-        `Vielen Dank, der Service wurde ausgeführt. Ihr neues Guthaben beträgt nun: ${this.balance}€`
-      );
+      return `Vielen Dank, der Service wurde ausgeführt. Ihr neues Guthaben beträgt nun: ${this.balance}€`;
     }
   },
 
   withdrawMoney() {
     let drawService = Number(prompt("Wieviel Geld möchten Sie abheben?"));
     if (drawService > savingsAccount.balance) {
-      console.log(
-        `Es tut mir leid, aber Sie möchten mehr Geld abheben, als Sie besitzen. Sie können maximal ${this.balance}€ abheben.`
-      );
+      return `Es tut mir leid, aber Sie möchten mehr Geld abheben, als Sie besitzen. Sie können maximal ${this.balance}€ abheben.`;
     } else {
       this.balance -= drawService;
-      console.log(
-        `Vielen Dank - Das Geld wurde abgebucht und Ihr neuer Kontostand beträgt nun: ${this.balance}€.`
-      );
+      return `Vielen Dank - Das Geld wurde abgebucht und Ihr neuer Kontostand beträgt nun: ${this.balance}€.`;
     }
   },
 
   getBalance() {
-    console.log(`Ihr aktuelles Guthaben beträgt: ${this.balance}€`);
+    return `Ihr aktuelles Guthaben beträgt: ${this.balance}€`;
   },
 };
 
@@ -92,11 +76,14 @@ if (chooseAccount === "Girokonto") {
     "Wie kann ich Ihnen weiterhelfen? Geld einzahlen, Geld abheben oder Kontostand abfragen?"
   );
   if (service === "Geld abheben") {
-    bankAccount.withdraw();
+    const message = bankAccount.withdrawMoney();
+    console.log(message);
   } else if (service === "Geld einzahlen") {
-    bankAccount.deposit();
+    const message = bankAccount.depositMoney();
+    console.log(message);
   } else if (service === "Kontostand abfragen") {
-    bankAccount.getBalance();
+    const message = bankAccount.getBalance();
+    console.log(message);
   } else {
     console.log(
       "Tut mir Leid, ich habe Sie nicht verstanden - Bitte sagen Sie 'Geld einzahlen' - 'Geld abheben' oder 'Kontostand abfragen'"
@@ -107,11 +94,14 @@ if (chooseAccount === "Girokonto") {
     "Wie kann ich Ihnen weiterhelfen? Geld einzahlen, Geld abheben oder Kontostand abfragen?"
   );
   if (service === "Geld abheben") {
-    savingsAccount.withdraw();
+    const message = savingsAccount.withdrawMoney();
+    console.log(message);
   } else if (service === "Geld einzahlen") {
-    savingsAccount.deposit();
+    const message = savingsAccount.depositMoney();
+    console.log(message);
   } else if (service === "Kontostand abfragen") {
-    savingsAccount.getBalance();
+    const message = savingsAccount.getBalance();
+    console.log(message);
   } else {
     console.log(
       "Tut mir Leid, ich habe Sie nicht verstanden - Bitte sagen Sie 'Geld einzahlen' - 'Geld abheben' oder 'Kontostand abfragen'"
