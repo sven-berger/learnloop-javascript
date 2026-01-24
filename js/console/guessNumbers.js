efcac370cc8a1d08244c6d70087c67c081853b96
Sven Oliver Berger <bergersvenoliver@gmail.com>
2026-01-24 17:34:03 +0100

    finish guessNumbers

--- js/console/guessNumbers.js
+++ js/console/guessNumbers.js
@@ -3,6 +3,9 @@
 let startNumber = 0;
 let endNumber = 0;
 
+let randomNumber = 0;
+let yourNumber = 0;
+
 function generateRandom(startNumber, endNumber) {
   startNumber = Number(prompt("Bitte wähle die Startzahl:"));
   endNumber = Number(prompt("Bitte wähle die Endzahl:"));
@@ -12,13 +15,41 @@ function generateRandom(startNumber, endNumber) {
   } else if (startNumber >= endNumber) {
     console.log("Die Startzahl muss kleiner als die Endzahl sein!");
   } else {
-    let randomNumber =
+    randomNumber =
       Math.floor(Math.random() * (endNumber - startNumber + 1)) + startNumber;
 
-    console.log(
-      `Zufallszahl zwischen ${startNumber} und ${endNumber}: ${randomNumber}`,
+    return randomNumber;
+  }
+}
+
+function chooseYourNumber(yourNumber) {
+  yourNumber = prompt("Bitte gib die Zahl an, mit der du raten möchtest!");
+  return yourNumber;
+}
+
+function playTheGame(yourNumber, randomNumber) {
+  yourNumber = Number(chooseYourNumber(yourNumber));
+  if (yourNumber < randomNumber) {
+    alert(`${yourNumber} ist zu niedrig! Du musst höher raten!`);
+    return playTheGame(yourNumber, randomNumber);
+  } else if (yourNumber > randomNumber) {
+    alert(`${yourNumber} ist zu hoch! Du musst niedriger raten!`);
+    return playTheGame(yourNumber, randomNumber);
+  } else {
+    let youWin = prompt(
+      "Herzlichen Glückwunsch, du hast die richtige Zahl geraten! Lust auf eine neue Runde? (Ja / Nein)",
     );
+    if (youWin === "Ja") {
+      randomNumber = generateRandom(startNumber, endNumber);
+      return playTheGame(yourNumber, randomNumber);
+    } else {
+      alert(
+        "Vielen Dank für das Mitmachen! Dann wünsche ich dir noch einen schönen restlichen Tag.",
+      );
+    }
   }
 }
 
 generateRandom(startNumber, endNumber);
+chooseYourNumber(yourNumber);
+playTheGame(yourNumber, randomNumber);
